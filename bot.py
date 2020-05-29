3d4fcccd85c489186f80a9b1ffb5e790dabe1fbb
joekifle <joey.kifle@gmail.com>
2020-05-29 16:28:52 +0300

    comment bot.polling()

--- bot.py
+++ bot.py
@@ -522,7 +522,7 @@ def shareContact_state(message, listOfcommands):
 
 
 
-bot.polling()
+#bot.polling()
 
 
 @server.route('/' + TOKEN, methods=['POST'])
@@ -539,4 +539,4 @@ def webhook():
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000))) 
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
